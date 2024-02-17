8dc3c5fdaf93748b36f85e3a31470fa8bdb3a490
Mickey Drury <drurymickey@gmail.com>
2024-02-16 19:30:13 -0500

    Save checkpoint; added modules though haven't implemented anything within them (2024-02-16)

---
+++ pyparse/core/bases.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import (
+    Protocol,
+    Callable,
+    Union,
+    Any,
+    List,
+    Dict,
+    Tuple,
+    LiteralString,
+    Type,
+    Optional,
+    runtime_checkable,
+)
+
+
+class 
+
+
+
+if __name__ == "__main__":
+    pass
